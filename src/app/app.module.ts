f9f6b4ec26fb08134bc38c705213a31e972eb585
bhagawatilal <bhagawatilal.patidar@gmail.com>
2021-07-04 13:20:22 +0530

    getMyLocation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
@@ -7,11 +9,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },Geolocation,AndroidPermissions],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,37 @@
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { Geolocation,  } from '@ionic-native/geolocation/ngx';
 import { Component } from '@angular/core';
-import { ToastController } from '@ionic/angular';
-
+import {  AlertController, LoadingController, ToastController,Platform } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor(public toastCtrl: ToastController) {}
-  async clicked(){
+  lati: any = '';  
+  longi: any = '';  
+  geolocationOptions = {
+    enableHighAccuracy: true,
+    timeout: 60000,
+    maximumAge: 0
+  };
+  constructor(public toastCtrl: ToastController , 
+    public platform: Platform,
+    private androidPermissions: AndroidPermissions,
+         public geolocation: Geolocation ,   public loadingController: LoadingController, public alertController: AlertController ) {}
+  requestLocationPermission() {
+    console.log(`this.requestLocationPermission flow started`)
+    this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION,
+      this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION]).then((result)=> {
+        console.log(`requestLocationPermission result:::${JSON.stringify(result)}`)
+      })
+      .catch(()=> {
+        this.showLoader('Error in permission')
+      })
+  }
+  async clicked(msg: string){
     const toast = await this.toastCtrl.create({  
-      message: 'Open Video clicked',  
+      message: msg,                       
       duration: 1000  ,
       position: 'middle',
       color: 'primary'
@@ -20,5 +40,49 @@ export class HomePage {
     toast.present();  
 
   }
-
-}
+  async myLocation(){
+    if (this.platform.is('android')) {
+      this.checkPermission();
+    } else {
+      this.getCurrentLocation();
+    }
+  }
+  async checkPermission(){
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
+     result => this.getCurrentLocation(),
+     err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION)
+      );
+  }
+   async getCurrentLocation() {  
+    const loading = await this.loadingController.create({  
+      message: 'Please wait...',  
+      });  
+    await loading.present();  
+  
+    this.geolocation.getCurrentPosition(this.geolocationOptions).then((resp) => {  
+     
+      loading.dismiss();  
+      this.lati = resp.coords.latitude;  
+      this.longi = resp.coords.longitude; 
+      this.showLoader(this.lati+','+this.longi) 
+      }, er => {  
+        // alert("error getting location")  
+        loading.dismiss();  
+        this.showLoader('Can not retrieve Location');  
+      }).catch((error) => {  
+      // alert('Error getting location'+JSON.stringify(error));  
+      loading.dismiss();  
+      this.showLoader('Error getting location - ' + JSON.stringify(error));  
+      });  
+  }  
+  
+ async showLoader(msg) {  
+    const alert = await this.alertController.create({  
+      message: msg,  
+      buttons: ['OK']  
+    });  
+  
+    await alert.present();  
+  }  
+ 
+}  
